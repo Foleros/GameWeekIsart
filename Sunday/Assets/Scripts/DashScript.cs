11d63907f5135c6abfd69bb7f4bbc5c3aa8958db
Foleros <v.biagini@student.isartdigital.com>
2018-04-30 16:14:40 +0200

    Fixed dash and Add Health

--- Sunday/Assets/Scripts/DashScript.cs
+++ Sunday/Assets/Scripts/DashScript.cs
@@ -12,28 +12,31 @@ public class DashScript : MonoBehaviour
 
     int direction;
 
+    bool Cool;
+
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         dashTime = startDashTime;
+        Cool = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (direction == 0)
         {
-            if (Input.GetAxis("P2_Horizontal") > 0.01f && Input.GetKeyDown(KeyCode.JoystickButton1))
+            if (Input.GetAxis("P2_Horizontal") > 0.01f && Input.GetKeyDown(KeyCode.JoystickButton1)&&Cool==false)
             {
                 direction = 1;
             }
 
-            else if (Input.GetAxis("P2_Horizontal") < -0.01f && Input.GetKeyDown(KeyCode.JoystickButton1))
+            else if (Input.GetAxis("P2_Horizontal") < -0.01f && Input.GetKeyDown(KeyCode.JoystickButton1) && Cool == false)
             {
                 direction = 2;
             }
 
-            else if (Input.GetAxis("Vertical") > 0.01f && Input.GetKeyDown(KeyCode.JoystickButton1))
+            else if (Input.GetAxis("Vertical") > 0.01f && Input.GetKeyDown(KeyCode.JoystickButton1) && Cool == false)
             {
                 direction = 3;
             }
@@ -45,6 +48,7 @@ public class DashScript : MonoBehaviour
                 direction = 0;
                 rb.velocity = new Vector2(0, 0);
                 dashTime = startDashTime;
+                
 
             }
             else
@@ -54,16 +58,31 @@ public class DashScript : MonoBehaviour
                 if (direction == 1)
                 {
                     rb.velocity = Vector2.right * dashSpeed;
+                    Cool = true;
+                    StartCoroutine(Cooldown(0.8f));
+
                 }
                 else if (direction == 2)
                 {
                     rb.velocity = Vector2.left * dashSpeed;
+                    Cool = true;
+                    StartCoroutine(Cooldown(0.8f));
+
                 }
                 else if (direction == 3)
                 {
                     rb.velocity = Vector2.up *2* dashSpeed;
+                    Cool = true;
+                    StartCoroutine(Cooldown(0.8f));
+
                 }
             }
         }
+    }
+
+    IEnumerator Cooldown(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Cool = false;
     }
 }
